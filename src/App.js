1fc1bee4a2cc7a79fd3b50ecdaa328909023894b
Merve Balci <mervebalci090@gmail.com>
2023-11-29 18:44:38 -0400

    Functioning the Add button to add new tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import { useState } from 'react';
+
+let counter = 0;
 
 export default function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  function addTask() {
+    const userInput = document.getElementById("userInput").value;
+
+    const task = {id: counter, name: userInput};
+    
+    setTaskList([...taskList, task]);
+    console.log(taskList)
+
+    counter = counter + 1;
+
+    document.getElementById("userInput").value = "";
+  }
+
   return (
     <div className="App">
       <h1>TODO LIST</h1>
-      <input placeholder="Add a new to do..."></input>
-      <button>Add</button>
+      <input id="userInput" placeholder="Add a new to do..."></input>
+      <button onClick={addTask}>Add</button>
     </div>
   );
 }
